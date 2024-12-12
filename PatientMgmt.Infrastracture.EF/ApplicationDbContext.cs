9ef28c7e0a15b0a46a2c4950e243ca438fe234ae
DouaeChmihi <douaeochmihi@gmail.com>
2024-12-12 22:38:09 +0100

    la création de ApplicationDbContext pour gérer la cnx à la db et les opérations sur les entités

---
+++ PatientMgmt.Infrastracture.EF/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PatientMgmt.Models;
+
+namespace PatientMgmt.Infrastracture.EF;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Patient> Patients { get; set; }
+    // Ajoutez d'autres DbSet pour vos entités ici
+}
